424bb45e458ecace6c751072cfe5a7d30d4ec25f
tuobaling <wunhueic+hub@gmail.com>
2022-10-07 18:58:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -138,40 +138,40 @@ def callback():
         return "OK"
 
 
-#@handler.add(MessageEvent, message=TextMessage)
-#def handle_message(event):
-#    get_message = event.message.text
-
-#    # Send To Line
-#    reply = TextSendMessage(text=f"{get_message}")
-#    line_bot_api.reply_message(event.reply_token, reply)
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.source.user_id =='Udeadbeefdeadbeefdeadbeefdeadbeef':
-        return 'OK'
-    #if event.message.text[:3] == "@翻英":
-    #    content = translate_text(event.message.text[3:], "en")
-    #    message = TextSendMessage(text=content)
-    #    line_bot_api.reply_message(event.reply_token, message)
-    #if event.message.text[:3] == "@翻日":
-    #    content = translate_text(event.message.text[3:] , "ja")
-    #    message = TextSendMessage(text=content)
-    #    line_bot_api.reply_message(event.reply_token, message)
-    #if event.message.text[:3] == "@翻中":
-    #    content = translate_text(event.message.text[3:] , "zh-tw")
-    #    message = TextSendMessage(text=content)
-    #    line_bot_api.reply_message(event.reply_token, message)
-    #if event.message.text[:2].upper() == "@K":
-    #    input_word = event.message.text.replace(" ","") #合併字串取消空白
-    #    stock_name = input_word[2:6] #2330
-    #    start_date = input_word[6:] #2020-01-01
-    #    content = plot_stcok_k_chart(IMGUR_CLIENT_ID,stock_name,start_date)
-    #    message = ImageSendMessage(original_content_url=content,preview_image_url=content)
-    #    line_bot_api.reply_message(event.reply_token, message)
+    get_message = event.message.text
 
-    else: line_bot_api.reply_message(event.reply_token,
-                                     TextSendMessage(text=event.message.text))
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
+
+#@handler.add(MessageEvent, message=TextMessage)
+#def handle_message(event):
+#    if event.source.user_id =='Udeadbeefdeadbeefdeadbeefdeadbeef':
+#        return 'OK'
+#    #if event.message.text[:3] == "@翻英":
+#    #    content = translate_text(event.message.text[3:], "en")
+#    #    message = TextSendMessage(text=content)
+#    #    line_bot_api.reply_message(event.reply_token, message)
+#    #if event.message.text[:3] == "@翻日":
+#    #    content = translate_text(event.message.text[3:] , "ja")
+#    #    message = TextSendMessage(text=content)
+#    #    line_bot_api.reply_message(event.reply_token, message)
+#    #if event.message.text[:3] == "@翻中":
+#    #    content = translate_text(event.message.text[3:] , "zh-tw")
+#    #    message = TextSendMessage(text=content)
+#    #    line_bot_api.reply_message(event.reply_token, message)
+#    #if event.message.text[:2].upper() == "@K":
+#    #    input_word = event.message.text.replace(" ","") #合併字串取消空白
+#    #    stock_name = input_word[2:6] #2330
+#    #    start_date = input_word[6:] #2020-01-01
+#    #    content = plot_stcok_k_chart(IMGUR_CLIENT_ID,stock_name,start_date)
+#    #    message = ImageSendMessage(original_content_url=content,preview_image_url=content)
+#    #    line_bot_api.reply_message(event.reply_token, message)
+
+#    else: line_bot_api.reply_message(event.reply_token,
+#                                     TextSendMessage(text=event.message.text))
 
 #@handler.add(MessageEvent, message=TextMessage)
 #def handle_message(event):
